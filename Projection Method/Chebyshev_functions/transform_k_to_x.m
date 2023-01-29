8e5c84f5d8f5317e4d03fcb07cdd7b27cf1527a2
Panagiotis Veneris <Panagiotis.Veneris@liverpool.ac.uk>
2023-01-29 19:51:53 +0000

    Create transform_k_to_x.m

---
+++ Projection Method/Chebyshev_functions/transform_k_to_x.m
@@ -0,0 +1,17 @@
+function x =  transform_k_to_x(k,m,p)
+
+%==================================================================================================================
+% Panagiotis Veneris, U of Liverpool, November 2022
+
+% Maps capital nodes (gridpoints) k (k_i in [p.kmin,p.kmax]) to the domain of chebyshev nodes x (x_i in [-1,1])
+
+% Input:  k:  {k_i}_i=1...m; 
+%         m:  # nodes at which we will approximate V(k)
+%         p:  struct array of model parameters 
+
+% Output: x: {x_i}_i=1...m in [-1,1] 
+%==================================================================================================================
+
+ x = cos(pi/(2*m)) * (2* ((k-p.kmin)./(p.kmax - p.kmin)) -1  );  % x(k) = {x_i}_i=1...m: function that maps capital gridpoints (k) to the domain [-1,1]
+
+end 
