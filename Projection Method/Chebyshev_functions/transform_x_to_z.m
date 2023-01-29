73a41995e775b07aad10fc5170364cc9e03efd08
Panagiotis Veneris <Panagiotis.Veneris@liverpool.ac.uk>
2023-01-29 19:53:02 +0000

    Create transform_x_to_z.m

---
+++ Projection Method/Chebyshev_functions/transform_x_to_z.m
@@ -0,0 +1,23 @@
+function z = transform_x_to_z(x,m,p) % x,m,p : inputs, k : output
+
+%=========================================================================================================================
+% Panagiotis Veneris, U of Liverpool, November 2022
+
+% Map Chebyshev nodes x_i from [-1,1] to the domain of the gridpoints for productivity [p.zmin,p.zmax]
+
+% In other words, it rescales Chebyshev nodes from [-1,1] to [p.zmin,p.zmax]
+% In other words, it projects collocation points x in the z space (this
+% explanation comes from Villaverde - Caldara code)
+
+% Input:  x:  nx1 vector of nodes {x_i}_i=1,...m at which we will approximate V(z),
+%             i.e nodes (gridpoints) at which we will evaluate our Chebyshevcpolynomials T_x_z
+%         m:  # nodes at which we will approximate V(k)
+%         p:  struct array of model parameters 
+
+% Output: z: {z_i}_i=1...m; 
+%==========================================================================================================================
+
+% map the nodes x to z
+z = (1 + x .* sec(pi/(2*m))) * ((p.zmax - p.zmin)./2) + p.zmin;  % z(x): function that maps the nodes x (x in [-1,1]) to the domain of the gridpoints for productivity (z in [p.zmin,p.zmax])
+
+end
